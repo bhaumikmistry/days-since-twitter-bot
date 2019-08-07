c8e05ed24218539e90c05885c7f1fb95aa710f54
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2019-08-06 23:53:46 -0400

    update requests fail solution for opwnssl

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Set up a twitter bot with python and heroku
 + Get a text image with the text in the center
 + Get instagram account and Use InstagramApi to upload image
 + keep private imformation safe
+
+#links
+[earth lab](https://www.earthdatascience.org/courses/earth-analytics-python/using-apis-natural-language-processing-twitter/get-and-use-twitter-data-in-python/)
+[text minning](http://adilmoujahid.com/posts/2014/07/twitter-analytics/)
+[python-tweeter](https://github.com/bear/python-twitter)
+[set upheroku and twitter app](https://dev.to/emcain/how-to-set-up-a-twitter-bot-with-python-and-heroku-1n39)
+
+[Solution to requests module failure](https://stackoverflow.com/a/38854398)
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from InstagramAPI import InstagramAPI
-from secret import UN
-from secret import PW
 from days_since import data
 from generate_image import GenerateImage
+from caption_creator import CaptionCreator
+from tweet import tweetdata
+from inatgrampost import InstagramPost
 
 if __name__ == "__main__":
-    InstagramAPI = InstagramAPI(UN,PW)
-    if(InstagramAPI.login()):
-        print("login() oK")
         d = data()
         caption = d.get_string()
+
         gi = GenerateImage(caption)
         photo_path = gi.generate_image()
-        InstagramAPI.uploadPhoto(photo=photo_path,caption=caption)
+
+        cc = CaptionCreator()
+        edited_caption = "Days Since India Won A Cricket World Cup: "
+        edited_caption += caption
+        add_on = cc.get_caption_add_on()
+        edited_caption += add_on
+
+        td = tweetdata(edited_caption)
+        ip = InstagramPost(photo_path,edited_caption)
+
+        td.tweet()
+        ip.post()
+
