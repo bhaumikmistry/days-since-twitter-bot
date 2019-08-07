50e32c47e4bf53c336c169d8b96eba1b0ed00660
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2019-08-06 23:54:39 -0400

    add insta post with python

---
+++ inatgrampost.py
@@ -0,0 +1,15 @@
+from InstagramAPI import InstagramAPI
+from secret import UN
+from secret import PW
+
+
+class InstagramPost():
+    def __init__(self,photo,caption):
+        self.image = photo
+        self.text = caption
+    
+    def post(self):
+        instagramAPI = InstagramAPI(UN,PW)
+        if(instagramAPI.login()):
+            print("Image Posted with caption {}".format(self.text))
+            instagramAPI.uploadPhoto(photo=self.image,caption=self.text)
