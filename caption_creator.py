93811e42c9dce3ca2ed070f34b69634139878622
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2019-08-06 23:54:05 -0400

    added caption creator with new #

---
+++ caption_creator.py
@@ -0,0 +1,18 @@
+
+class CaptionCreator():
+    def __init__(self):
+        self.caption =""
+
+    def get_caption_add_on(self):
+        text = " "
+        text += "#india "
+        text += "#indiancricket "
+        text += "#CricketIndiaClub "
+        text += "#Cricket "
+        text += "#teamindia "
+        text += "#bleedblue "
+        text += "#cricketlover "
+        text += "#worldcup "
+        text += "#DSIWACWC"
+        self.caption = text
+        return self.caption
