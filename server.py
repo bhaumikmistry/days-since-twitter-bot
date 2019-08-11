49c4fc121bea4d993b08c58a68638a136fa2a396
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2019-08-11 16:41:30 -0400

    fix server

--- server.py
+++ server.py
@@ -1,16 +1,7 @@
 from os import environ
-from flask import Flask
-from days_since import data
-app = Flask(__name__)
+import flask
+app = flask.Flask(__name__)
 
-
-@app.route('/')
-def hello():
-    d = data()
-    caption = d.get_string()
-    edited_caption = "Days Since India Won A Cricket World Cup: "
-    edited_caption += caption
-    return edited_caption
-
-if __name__ == '__main__':
-    app.run(host= '0.0.0.0', port=environ.get('PORT'))
+@app.route("/")
+def index():
+    return "Hello Heruko"
