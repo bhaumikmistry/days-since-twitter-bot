04848591d17f7d8e63d14d4fbb1e36ae3dbe6c28
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2019-08-11 17:29:34 -0400

    edit server

--- server.py
+++ server.py
@@ -2,4 +2,13 @@ from os import environ
 from flask import Flask
 
 app = Flask(__name__)
-app.run(host='0.0.0.0', port=environ.get('PORT'))
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=environ.get('PORT'))
