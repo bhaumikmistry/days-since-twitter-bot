9f528731167f9ce370cd66ae1ae648e6280abe9a
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2019-08-11 16:04:38 -0400

    update server display

--- server.py
+++ server.py
@@ -1,5 +1,16 @@
 from os import environ
-from flask import flask
+from flask import Flask
+from days_since import data
+app = Flask(__name__)
 
-app = False(__name__)
-app.run(host='0.0.0.',port=environ.get('PORT'))
+
+@app.route('/')
+def hello():
+    d = data()
+    caption = d.get_string()
+    edited_caption = "Days Since India Won A Cricket World Cup: "
+    edited_caption += caption
+    return edited_caption
+
+if __name__ == '__main__':
+    app.run()
