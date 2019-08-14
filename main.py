6853a9f60d88f4ee126e8b0c156fd87ed5a06fb6
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2019-08-13 23:54:34 -0400

    update server heroku

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ from generate_image import GenerateImage
 from caption_creator import CaptionCreator
 from tweet import tweetdata
 from inatgrampost import InstagramPost
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-if __name__ == "__main__":
-        d = data()
-        caption = d.get_string()
+sched = BlockingScheduler()
 
-        gi = GenerateImage(caption)
-        photo_path = gi.generate_image()
+@sched.scheduled_job('cron', day='*', hour=0, minute=5)
+def timed_job():
+    print('This runs every day 00:05')
+    d = data()
+    caption = d.get_string()
 
-        cc = CaptionCreator()
-        edited_caption = "Days Since India Won A Cricket World Cup: "
-        edited_caption += caption
-        add_on = cc.get_caption_add_on()
-        edited_caption += add_on
+    gi = GenerateImage(caption)
+    photo_path = gi.generate_image()
 
-        td = tweetdata(edited_caption)
-        ip = InstagramPost(photo_path,edited_caption)
+    cc = CaptionCreator()
+    edited_caption = "Days Since India Won A Cricket World Cup: "
+    edited_caption += caption
+    add_on = cc.get_caption_add_on()
+    edited_caption += add_on
 
-        # td.tweet()
-        # ip.post()
+    td = tweetdata(edited_caption)
+    ip = InstagramPost(photo_path,edited_caption)
 
+    td.tweet()
+    ip.post()
+
+sched.start()
