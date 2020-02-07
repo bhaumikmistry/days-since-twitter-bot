4d55e9464c3e37c4b3e364d589f643c27b1e4b4f
Bhaumik Mistry <bhaumikpmistry@gmail.com>
2020-02-07 10:18:14 -0500

    Added new facts file to be used for tweets later with more facts

---
+++ facts.py
@@ -0,0 +1,18 @@
+from random import randint
+
+facts = [
+    "1st Highest Innings total, 413–5 (50 overs) v Bermuda, India won by 257 runs, 12th Match, Group B, ICC Cricket World Cup at Port of Spain, Mar 19 2007",
+    "2nd Highest Innings total, 373-6 (50 overs) v Sri Lanka, India won by 157 runs, 21st Match, ICC Cricket World Cup at Taunton, May 26 1999",
+    "3rd Highest Innings total, 370–4 (50 overs) v Bangladesh, India won by 87 runs, 1st Match, Group B (D/N), ICC Cricket World Cup at Dhaka, Feb 19 2011",
+    "4th Highest Innings total, 352–5 (50 overs) v Australia, India won by 36 runs, 14th match, ICC Cricket World Cup at The Oval, Jun 9 2019",
+    "5th Highest Innings total, 338 (49.5 overs) v England, Match tied, 11th Match, Group B (D/N), ICC Cricket World Cup at Bengaluru, Feb 27 2011",
+]
+
+
+# if __name__ == "__main__":
+#     length = len(facts)
+#     print(length)
+
+#     random_number = randint(0,length)
+#     print(random_number)
+
