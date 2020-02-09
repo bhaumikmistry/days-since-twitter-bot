f10ed6bdaffb1efb0e124df592795e05e39eaedc
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2020-02-09 18:22:01 -0500

    added a function which can load fact and write it to an image

--- generate_image.py
+++ generate_image.py
@@ -1,5 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 from days_since import Data
+from facts import facts as facts
+from random import randint
 
 class GenerateImage():
     def __init__(self,text_to_add):
@@ -37,10 +39,82 @@ class GenerateImage():
         image_data.save(filename)
         return filename
 
-# if __name__ == "__main__":
-#     d = data()
-#     caption = d.get_string()
-#     edited_caption = "Days Since India Won A Cricket World Cup: "
-#     edited_caption += caption
-#     gi = GenerateImage(edited_caption)
-#     photo_path = gi.generate_image_test()
+    def generate_image_with_facts(self,number,fact):
+        # image load white image
+        image_data = Image.open('data/white.jpg')
+        draw = ImageDraw.Draw(image_data)
+
+        # create font for the number text
+        font = ImageFont.truetype('fonts/OpenSans-Light.ttf',size=50)
+        x,y = (800,700)
+
+        # message of the number
+        message = number 
+        color = 'rgb(150,150,150)'
+        w, h = draw.textsize(message,font=font)
+        draw.text(((x-w)/2,(y-h)/2),message,fill=color,font=font)
+
+        # max length of line at size 24 is 60
+        max_line_length = 60
+        word_list = fact.split()
+        sentence = []
+        line = str("")
+        for word in word_list:
+            if 1+len(line)+len(word) > max_line_length:
+                sentence.append(line)
+                line = word
+            else:
+                if len(line)>0:
+                    line = line + " " + word
+                else:
+                    line = word
+        if len(line)>0:
+            sentence.append(line)
+        
+        # print lines to the image
+        font_size = 24
+        starting_point = [800,800]
+        line_space_diff = font_size*2
+        for line in sentence:
+            font = ImageFont.truetype('fonts/OpenSans-Light.ttf',size=font_size)
+            x,y = (starting_point[0],starting_point[1])
+            starting_point[1]+=line_space_diff
+            message = line
+            print(len(message))
+            color = 'rgb(50,50,50)'
+            w, h = draw.textsize(message,font=font)
+            draw.text(((x-w)/2,(y-h)/2),message,fill=color,font=font)
+
+        # create font for the fact text
+        # font = ImageFont.truetype('fonts/OpenSans-Light.ttf',size=24)
+        # x,y = (800,800)
+        # message = fact
+        # print(len(message))
+        # color = 'rgb(50,50,50)'
+        # w, h = draw.textsize(message,font=font)
+        # draw.text(((x-w)/2,(y-h)/2),message,fill=color,font=font)
+        # x,y = (800,848)
+        # message = fact
+        # print(len(message))
+        # color = 'rgb(50,50,50)'
+        # w, h = draw.textsize(message,font=font)
+        # draw.text(((x-w)/2,(y-h)/2),message,fill=color,font=font)
+
+        filename = "data/{}.jpg".format("test")
+        image_data.save(filename)
+
+
+if __name__ == "__main__":
+    d = Data()
+    caption = d.get_string()
+    edited_caption = "01345 78901 34567 90123 56789 12345 78901 34567 90123 56789012 12345 78901 34567 90123 6789 12345 78901 34567 78901 34567 90123 6789 12345 78901 34567"
+    gi = GenerateImage(edited_caption)
+
+    length = len(facts)
+    print('Number of facts -> ' + str(length))
+    random_number = randint(0,length-1)
+    print('random fact id -> '+ str(random_number))
+    print('Fact -> ' + facts[random_number])
+    fact = facts[random_number]
+
+    photo_path = gi.generate_image_with_facts("3251",fact)
